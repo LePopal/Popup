d19e519f4719db54563a6bd607730459e62b1141
Popal <popal@sboub.com>
2026-01-11 21:49:05 +0100

    1st real commit

---
+++ src/Popup.Updater.Launcher/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Popup.Updater.Launcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
